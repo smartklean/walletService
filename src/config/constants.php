1e3271f815a7833309b7cacf35e7c53839c3e5ee
Jesse Essang <jesse@cashenvoy.com>
2021-03-12 21:31:01 +0100

    Added constants.php to config folder, added support for unit tests and updated readme.md

---
+++ src/config/constants.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+  /*
+  |--------------------------------------------------------------------------
+  | Default Database Constants
+  |--------------------------------------------------------------------------
+  |
+  | Here you may specify default values for the database connection parameters
+  | in your database.php configuration file.
+  |
+  */
+
+  'db' => [
+    'host' => '127.0.0.1',
+    'port' => 3306,
+    'database' => 'forge',
+    'username' => 'forge',
+    'password' => '',
+    'socket' => '',
+    'charset' => 'utf8',
+    'collation' => 'utf8_general_ci',
+    'prefix' => '',
+    'strict' => true,
+    'engine' => 'InnoDB',
+    'timezone' => '+01:00'
+  ],
+];
