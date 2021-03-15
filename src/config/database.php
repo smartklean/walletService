98e1dfb429b81a9a199f5fe104ff42dec2176cc9
Jesse Essang <jesse@cashenvoy.com>
2021-03-15 18:04:32 +0100

    Updated config/database.php to eliminate code duplicity.

--- src/config/database.php
+++ src/config/database.php
@@ -34,20 +34,13 @@ return [
     */
 
     'connections' => [
-
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => env('DB_PREFIX', config('constants.db.prefix')),
-        ],
-
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('LUMENWS_DB_HOST', config('constants.db.host')),
-            'port' => env('LUMENWS_DB_PORT', config('constants.db.port')),
-            'database' => env('LUMENWS_DB_DATABASE', config('constants.db.database')),
-            'username' => env('LUMENWS_DB_USERNAME', config('constants.db.username')),
-            'password' => env('LUMENWS_DB_PASSWORD', config('constants.db.password')),
+            'host' => env('LUMENWS_DB_HOST'),
+            'port' => env('LUMENWS_DB_PORT'),
+            'database' => env('LUMENWS_DB_DATABASE'),
+            'username' => env('LUMENWS_DB_USERNAME'),
+            'password' => env('LUMENWS_DB_PASSWORD',
             'unix_socket' => env('LUMENWS_DB_SOCKET', config('constants.db.socket')),
             'charset' => env('LUMENWS_DB_CHARSET', config('constants.db.charset')),
             'collation' => env('LUMENWS_DB_COLLATION', config('constants.db.collation')),
@@ -59,11 +52,11 @@ return [
 
         'mysql_test' => [
             'driver' => 'mysql',
-            'host' => env('LUMENWS_TEST_DB_HOST', config('constants.db.host')),
-            'port' => env('LUMENWS_TEST_DB_PORT', config('constants.db.port')),
-            'database' => env('LUMENWS_TEST_DB_DATABASE', config('constants.db.database')),
-            'username' => env('LUMENWS_TEST_DB_USERNAME', config('constants.db.username')),
-            'password' => env('LUMENWS_TEST_DB_PASSWORD', config('constants.db.password')),
+            'host' => env('LUMENWS_TEST_DB_HOST'),
+            'port' => env('LUMENWS_TEST_DB_PORT'),
+            'database' => env('LUMENWS_TEST_DB_DATABASE'),
+            'username' => env('LUMENWS_TEST_DB_USERNAME'),
+            'password' => env('LUMENWS_TEST_DB_PASSWORD'),
             'unix_socket' => env('LUMENWS_TEST_DB_SOCKET', config('constants.db.socket')),
             'charset' => env('LUMENWS_TEST_DB_CHARSET', config('constants.db.charset')),
             'collation' => env('LUMENWS_TEST_DB_COLLATION', config('constants.db.collation')),
@@ -72,31 +65,6 @@ return [
             'engine' => env('LUMENWS_TEST_DB_ENGINE', config('constants.db.engine')),
             'timezone' => env('LUMENWS_TEST_DB_TIMEZONE', config('constants.db.timezone')),
         ],
-
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', config('constants.db.host')),
-            'port' => env('DB_PORT', 5432),
-            'database' => env('DB_DATABASE', config('constants.db.database')),
-            'username' => env('DB_USERNAME', config('constants.db.username')),
-            'password' => env('DB_PASSWORD', config('constants.db.password')),
-            'charset' => env('DB_CHARSET', config('constants.db.charset')),
-            'prefix' => env('DB_PREFIX', config('constants.db.prefix')),
-            'schema' => env('DB_SCHEMA', 'public'),
-            'sslmode' => env('DB_SSL_MODE', 'prefer'),
-        ],
-
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', 1433),
-            'database' => env('DB_DATABASE', config('constants.db.database')),
-            'username' => env('DB_USERNAME', config('constants.db.username')),
-            'password' => env('DB_PASSWORD', config('constants.db.password')),
-            'charset' => env('DB_CHARSET', config('constants.db.charset')),
-            'prefix' => env('DB_PREFIX', config('constants.db.prefix')),
-        ],
-
     ],
 
     /*
@@ -111,43 +79,4 @@ return [
     */
 
     'migrations' => 'migrations',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Redis Databases
-    |--------------------------------------------------------------------------
-    |
-    | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer set of commands than a typical key-value systems
-    | such as APC or Memcached. Laravel makes it easy to dig right in.
-    |
-    */
-
-    'redis' => [
-
-        'client' => env('REDIS_CLIENT', 'phpredis'),
-
-        'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'lumen'), '_').'_database_'),
-        ],
-
-        'default' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', config('constants.db.host')),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
-        ],
-
-        'cache' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', config('constants.db.host')),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
-        ],
-
-    ],
-
 ];
